88497e64671c72cce41a1f5da92a070f9d794974
maud bouyala <maud.bouyala@yahoo.fr>
2023-02-22 15:22:43 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+
+5.times do
+  Review.create(
+    content: Faker::Movie.quote,
+    rating: rand(1..5)
+  )
+end
