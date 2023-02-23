0b6491134be90764d0817f8721f50d411e1189b6
maud bouyala <maud.bouyala@yahoo.fr>
2023-02-23 11:42:06 +0100

    rails new

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,3 +3,6 @@ class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
+
+
+
